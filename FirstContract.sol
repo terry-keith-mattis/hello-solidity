3273fc66cc099669882c15a7d82064eac29dcc56
terry-keith-mattis <80283517+terry-keith-mattis@users.noreply.github.com>
2021-11-21 20:02:54 +0530

    Create FirstContract.sol

---
+++ FirstContract.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title MyContract
+ * @dev Exploring different types of state variables
+ */
+contract FirstContract {
+    string public myString = "Hello, Solidity !";
+    int public myInt = -24;
+    uint public myUint = 24;
+    address public myAddress = 0x74a54eb8bfD7e5Ef41CC18d26e4eFa4eAD415F75;
+    
+}
